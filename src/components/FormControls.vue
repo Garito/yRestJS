29f181957ba35c4083ecf86f804751012b375063
Garito <garito@gmail.com>
2019-06-11 17:32:40 +0200

    Put the form's controls in a component
    So the spreadForm can spread it too

---
+++ src/components/FormControls.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="columns" :class="name">
+    <div class="column">
+      <button class="button is-primary is-fullwidth" type="button" @click="$emit('ok')">{{ action }}</button>
+    </div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'FormControls',
+    props: { name: String, action: String }
+  }
+</script>

c86f6e03a968561e7301538481b8277718d1dd8c
Garito <garito@gmail.com>
2020-02-20 00:14:50 +0100

    Allow slots, events and i18n

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -6,16 +6,25 @@ export default {
   props: { tmpl: String, ctx: Object },
   render (cE) {
     let el = Vue.compile(this.tmpl)
-    el = new Vue({
+    let opts = {
       data: this.ctx,
       methods: {
         url: this.$store.getters.url
       },
       render: el.render,
       staticRenderFns: el.staticRenderFns,
-      router: this.$router,
-    })
-    el.$slots = this.$slots
+      router: this.$router
+    }
+    if (this._i18n) {
+      opts.i18n = this._i18n
+    }
+
+    el = new Vue(opts)
+    el.$scopedSlots = this.$scopedSlots
+    for (let [name, callback] of Object.entries(this.$listeners)) {
+      el._events[name] = [callback]
+    }
+
     el.$mount()
 
     return el._vnode
